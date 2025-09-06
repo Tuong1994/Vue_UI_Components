1c6bafe83e0c339c1d84fda7722ef541315f8d97
Tuong1994 <77843790+Tuong1994@users.noreply.github.com>
2025-09-06 15:11:04 +0000

    update css Button, Tabs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
-import { UI, Control } from '@/components'
+import { Section, Tabs } from '@/components/UI'
+import { iconName } from '@/components/UI/Icon/constant'
+import type { TabsItems } from '@/components/UI/Tabs/type'
 
-const { Section, Divider } = UI
-
-const {} = Control
+const tabItems: TabsItems = [
+  { id: 'item-1', label: 'Item 1', comName: 'item-1', labelIcon: { iconName: iconName.USER } },
+  { id: 'item-2', label: 'Item 2', comName: 'item-2', labelIcon: { iconName: iconName.USER } },
+  { id: 'item-3', label: 'Item 3', comName: 'item-3', labelIcon: { iconName: iconName.USER } }
+]
 </script>
 
 <template>
-  <Section :rootStyle="{ width: '100%', height: '500px' }">
-    <Divider dashed placement="center">Divider</Divider>
-    <Divider dashed type="vertical" placement="center">Divider</Divider>
-    <span>Content</span>
-    <Divider dashed type="vertical" placement="center">Divider</Divider>
-    <span>Content</span>
-    <Divider dashed type="vertical" placement="center">Divider</Divider>
+  <Section>
+    <Tabs :items="tabItems">
+      <template #content="tab">
+        <div v-if="tab.content.comName === 'item-1'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus tempore laborum labore, iure
+          deleniti neque beatae asperiores suscipit cum adipisci excepturi impedit architecto culpa mollitia
+          ratione doloribus modi quisquam autem enim perspiciatis molestias necessitatibus qui cupiditate.
+          Expedita, saepe fugiat autem reprehenderit tempore dolor cumque sit corporis numquam, similique
+          excepturi laborum.
+        </div>
+        <div v-if="tab.content.comName === 'item-2'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit sapiente id cupiditate ducimus
+          laudantium, quo aspernatur nam eius qui iste!
+        </div>
+        <div v-if="tab.content.comName === 'item-3'">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod eius animi nihil consectetur quae
+          illum unde ab totam magni nesciunt.
+        </div>
+      </template>
+    </Tabs>
   </Section>
 </template>
